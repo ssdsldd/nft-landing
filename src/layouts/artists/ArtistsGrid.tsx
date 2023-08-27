f9df9c67984c977821f9a4ccec234a94d07badfd
ssdsldd01~ <noryaivanov@gmail.com>
2023-08-27 14:12:16 +0600

    added some sections

---
+++ src/layouts/artists/ArtistsGrid.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import artistImg1 from "../../assets/popular/popular1.webp";
+import artistImg2 from "../../assets/popular/popular2.webp";
+import artistImg3 from "../../assets/popular/popular3.webp";
+import artistImg4 from "../../assets/popular/popular4.webp";
+import artistImg5 from "../../assets/popular/popular5.webp";
+import ownerImg1 from "../../assets/popular/owner1.webp";
+import ownerImg2 from "../../assets/popular/owner2.webp";
+import ownerImg3 from "../../assets/popular/owner3.webp";
+import ownerImg4 from "../../assets/popular/owner4.webp";
+import ownerImg5 from "../../assets/popular/owner5.webp";
+
+
+const artistsItems = [
+    {
+        srcBg: artistImg1,
+        srcProfile: ownerImg1,
+        name: "Osvaldo Percy"
+    },
+
+    {
+        srcBg: artistImg2,
+        srcProfile: ownerImg2,
+        name: "Ranson Sqiure"
+    },
+    
+
+    {
+        srcBg: artistImg3,
+        srcProfile: ownerImg3,
+        name: "Cristio leo"
+    },
+    
+
+    {
+        srcBg: artistImg4,
+        srcProfile: ownerImg4,
+        name: "Sebastian waltan"
+    },
+    
+    {
+        srcBg: artistImg5,
+        srcProfile: ownerImg5,
+        name: "Abraham Zack"
+    },
+]
+
+export const ArtistsGrid: React.FC = () => {
+    return(
+        <StyledAGrid>
+            
+        </StyledAGrid>
+    )
+}
+
+const StyledAGrid = styled.div`
+    
+`
